f4743c4b7640a5f79740d351f379e90ce331273f
bunizao <102936102+bunizao@users.noreply.github.com>
2025-04-26 21:07:22 +0800

    Update merge.py

--- Chores/engineering/merge.py
+++ Chores/engineering/merge.py
@@ -40,7 +40,7 @@ for info in sgmodule_info:
         for section, text in matches:
             if section in sections and section != "MITM":
                 if section == "Rule":
-                    cleaned_text = re.sub(r"(?:,\s*|-\s*)(REJECT|DIRECT|REJECT-DROP|REJECT-TINYGIF)\s*$", "", text, flags=re.I).strip()
+                    cleaned_text = re.sub(r'(?:,\s*|-\s*)(REJECT(?:-(?:DROP|TINYGIF|NO-DROP))?|DIRECT)\b', '', text, flags=re.I).strip()
                     sections["Rule"].append(cleaned_text)
                     print(f"[Debug] Added cleaned Rule content from {info['header']}: {cleaned_text}")
                 else:
